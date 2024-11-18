9fd7249473a3d0fd0d3b13e1b5a74f98d38d5421
Khensani Chauke <khchaukjhb024@student.wethinkcode.co.za>
2024-11-18 11:06:26 +0200

    data

--- functions.py
+++ functions.py
@@ -6,7 +6,14 @@ def reverse_list(lst):
     :param lst: List of integers.
     :return: A list with elements in reverse order.
     """
-    pass  # Implement this
+    reverse_list = []
+    list = [20, 40, 60, 80, 100]
+    for n in range (len(lst) -1, -1, -1):
+        reversed_list.append(lst[n])
+        return reversed_list
+    reversed_list = reverse_list(list)
+
+    print(reversed_list)
 
 def count_occurrences(lst, element):
     """
@@ -15,7 +22,12 @@ def count_occurrences(lst, element):
     :param element: Element to count.
     :return: Integer count of occurrences.
     """
-    pass  # Implement this
+    list = [10, 20, 30, 40, 40, 20, 50]
+    count = 0
+    for item in lst:
+        if item == element:
+            count += 1
+    return count
 
 def get_keys_with_value(dct, value):
     """
@@ -24,7 +36,10 @@ def get_keys_with_value(dct, value):
     :param value: Value to find.
     :return: List of keys.
     """
-    pass  # Implement this
+    keys = []
+
+
+    return keys
 
 def merge_sorted_lists(lst1, lst2):
     """
@@ -41,7 +56,21 @@ def find_second_largest(numbers):
     :param numbers: List of integers.
     :return: The second largest integer.
     """
-    pass  # Implement this
+    numbers = [2, 4, 6, 8, 10, 20]
+
+    if len(numbers) <2:
+        raise ValueError ("must have more than one number")
+    for num in numbers[2:]:
+        if num > largest:
+            second_largest = largest
+            largest = num
+
+        elif num > second_largest and largest:
+            second_largest = num
+            return second_largest
+        
+    print(second_largest)
+
 
 def is_anagram(str1, str2):
     """
@@ -55,7 +84,27 @@ def is_anagram(str1, str2):
     :param str2: Second string.
     :return: True if the strings are anagrams, False otherwise.
     """
-    pass  # Implement this
+    str1 = "Listen"
+    str2 = "Silent"
+
+    if len(str1) != len(str2):
+        return False
+    
+    char_count = {}
+    for char in str1:
+        char_count[char] = char_count.get(char, 0) + 1
+
+    for char in str2:
+        if char not in char_count:
+            return False
+        char_count[char] -= 1
+
+    else:
+        return True
+
+
+    result = is_anagram(str1, str2)
+    print(result)
 
 
 def flatten_list(nested_list):
@@ -64,7 +113,13 @@ def flatten_list(nested_list):
     :param nested_list: List of lists.
     :return: A flat list with all elements.
     """
-    pass  # Implement this
+    nested_list = [a, b, c [d, e, f], g, h, [i, j[k,l]]]
+    flat_list = []
+    for elements in nested_list:
+        flat_list.extend(flatten_list(elements))
+    else:
+        flat_list.append(elements)
+    return flat_list
 
 
 def remove_duplicates(lst):
